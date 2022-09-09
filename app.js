d984eb176494b5fc6c763f1c2d2b2062adaa6f2b
dlsdk0601 <dlsdk0601@gmail.com>
2022-09-09 14:12:42 +0900

    issue: data argument must be of type string

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 // 서버 생성
 const http = require("http");
-const fs = require("Fs");
+const fs = require("fs");
 
 // http가 만든 서버를 변수에 저장해야합니다.
 
 const server = http.createServer((req, res) => {
-  // res write ::::::::::::::::::::::::::::::::::::::::
-  // res.setHeader("Content-Type", "text/html");
-  // res.write("<html>");
-  // res.write("<head>");
-  // res.write("<title>My First Page</title>");
-  // res.write("</head>");
-  // res.write("<body>");
-  // res.write("<h1>Hello from Server!</h1>");
-  // res.write("</body>");
-  // res.write("</html>");
-  // res.end();
-  // :::::::::::::::::::::::::::::::::::::::::::::::::
   // router 요청 처리 ::::::::::::::::::::::::::::::::
   const url = req.url;
   const method = req.method;
@@ -36,13 +24,20 @@ const server = http.createServer((req, res) => {
 
   if (url === "/message" && method === "POST") {
     const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
+    req.on(
+      "data",
+      (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      },
+      (err) => console.log(err)
+    );
+    return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split("=")[1];
+
+      // writeFile method도 존재함
+      // sync는 동기화를 의미 => 비동기와 반대로 파일이 생성될때까지 머무르고 다음을 실행시킴
       fs.writeFileSync("message.txt", message);
       res.statusCode = 302;
       res.setHeader("Location", "/");
