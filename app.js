aa8bcfb583578bff070267988e3b167649de7a7c
dlsdk0601 <dlsdk0601@gmail.com>
2022-09-10 20:33:00 +0900

    feat: middleware res.send

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log("middle1");
+  next();
 });
 
 app.use((req, res, next) => {
   // 첫번째 미들웨어에서 next()를 하지 않으면 넘어오지 않음
   console.log("middle2");
+  res.send("<h1>Hello world</h1>");
 });
 
 const server = http.createServer(app);
