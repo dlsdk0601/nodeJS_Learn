13777ecf7ee0ffbdd79ab5891fe038df69826b50
dlsdk0601 <dlsdk0601@gmail.com>
2022-09-11 14:01:38 +0900

    feat: html file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // router 순서가 매우 중요하기에, 잘 고려해서 순서대로 작성할것.
-app.use(admonRoutes);
+app.use("/admin", admonRoutes);
 app.use(shopRoutes);
 
 // 404 error
