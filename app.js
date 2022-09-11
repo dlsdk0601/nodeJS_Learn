f4b97b285ee1599c046cac5fa73c475c41145edd
dlsdk0601 <dlsdk0601@gmail.com>
2022-09-11 14:42:00 +0900

    feat: public

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const admonRoutes = require("./routes/admin");
@@ -7,13 +8,19 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// html에 해당하는 css 파일이 다운 받아지지 않음
+// public폴더는 공개 폴더이므로 여기에 css같은 파일을 넣어놓음. 그외 라우팅은 express에서 라우팅 처리하려고 시도함
+// express.static 정적으로 서비스하기 원하는 폴더 경로를 입력하면 된다. (바로 읽기 권한은 허용하고자하는 폴더)
+// 이제 .css나 .js파일을 찾으려할때는 자동으로 public 폴더로 포워딩한다.
+app.use(express.static(path.join(__dirname, "public")));
+
 // router 순서가 매우 중요하기에, 잘 고려해서 순서대로 작성할것.
 app.use("/admin", admonRoutes);
 app.use(shopRoutes);
 
 // 404 error
 app.use((req, res, next) => {
-  res.status(404).send("<h1>page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello world</h1>");
+  // 절대 경로로 작성해야한다.
+  // __dirname 현재 서버가 돌아가는 절대 경로 (이 파일에서 불러오면 routes까지 경로를 의미 )
+  // 절대 경로에서 ../views/shop.html을 붙여주는 역할을 join이 한다
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
-module.exports = Router;
+module.exports = router;
