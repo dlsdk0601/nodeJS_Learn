d16d459493407e24f4f1d7aa52799a6576d3bb98
dlsdk0601 <dlsdk0601@gmail.com>
2022-09-24 22:54:50 +0900

    fix: cart model 다시 squelize로 정의

--- models/cart.js
+++ models/cart.js
@@ -1,80 +1,13 @@
-import fs from "fs";
-import path from "path";
-import dotenv from "dotenv";
+import Sequelize from "sequelize";
+import db from "../utils/databse.js";
 
-dotenv.config();
+const Cart = db.define("cart", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+});
 
-const __dirname = path.resolve();
-
-const p = path.join(__dirname, "data", "cart.json");
-
-export default class Cart {
-  // 장바구니를 해당 페이지에 올때마다 새로 생성하는게 아니라, 만들어진 애를 계속 보여주는거라서 constructor가 아닌 static을 사용
-
-  constructor() {}
-
-  static addProduct(id, productPrice) {
-    // 기존의 장바구니를 조회
-    // 새로운 상품이면 추가 및 수량 증가
-    fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
-
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-      const existingProduct = cart.products[existingProductIndex];
-
-      let updateProduct;
-
-      if (existingProduct) {
-        updateProduct = { ...existingProduct, qyt: existingProduct.qty + 1 };
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updateProduct;
-      } else {
-        updateProduct = {
-          id,
-          qty: 1,
-        };
-        cart.products = [...cart.products, updateProduct];
-      }
-
-      cart.totalPrice = cart.totalPrice + productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-
-  static deleteProduct(id, price) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return;
-      }
-      const updatedCart = { ...JSON.parse(fileContent) };
-      const product = updatedCart.products.find((item) => item.id === id);
-      if (!product) {
-        return;
-      }
-      const productQty = product.qty;
-      updatedCart.products = updatedCart.products.filter(
-        (item) => item.id !== id
-      );
-      updatedCart.totalPrice -= price * productQty;
-
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-
-  static getCart(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      const cart = JSON.parse(fileContent);
-      if (err) cb(null);
-      cb(cart);
-    });
-  }
-}
+export default Cart;
