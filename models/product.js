848edf883200d2220ec6d871d3a397bd3ec25a34
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-12 00:19:11 +0900

    fix: 수정 시, id 분기처리

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
   save() {
     const db = mongoConnect.getDB();
@@ -50,6 +50,17 @@ class Product {
       .next()
       .then((product) => {
         return product;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = mongoConnect.getDB();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log(result);
       })
       .catch((err) => console.log(err));
   }
