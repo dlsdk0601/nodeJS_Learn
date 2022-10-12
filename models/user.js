cc73688759f123e916a8388a0a1e1bacbec260a6
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-12 22:37:59 +0900

    fix: user model에 카트 리스트 추가

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 import mongodb from "mongodb";
-import mongoConnect from "../utils/databse";
+import mongoConnect from "../utils/databse.js";
 
 const ObjectId = mongodb.ObjectId;
+
 class User {
-  constructor(userName, email) {
+  constructor(userName, email, cart, id) {
     this.name = userName;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -13,9 +16,32 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
+  addToCart(product) {
+    // -1이 나오면 없는 상품
+    // const cartProduct = this.cart.items.findIndex(
+    //   (item) => item._id === product._id
+    // );
+    const db = mongoConnect.getDB();
+    const updateCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    };
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updateCart } }
+      );
+  }
+
   static findById(userId) {
     const db = mongoConnect.getDB();
-    return db.collection("users").find({ _id: new ObjectId(userId) });
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
