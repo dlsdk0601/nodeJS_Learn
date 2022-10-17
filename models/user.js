17eb34b15e50b94736fdcdf773dd3ed55232c28c
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-18 01:05:28 +0900

    refactor: userSchema 에 addToCart와 removeFromCart 추가

--- models/user.js
+++ models/user.js
@@ -25,6 +25,48 @@ const userSchema = new Schema({
   },
 });
 
+// userModel에는 직접 addTocart를 작성했지만, 여기서는 userSchema에 methods로 로직을 추가할수있다.
+userSchema.methods.addToCart = function (product) {
+  let quantity = 1;
+  const updateCartItem = [...this.cart.items];
+  const cartProductIndex = this.cart.items.findIndex((item) => {
+    return item.productId.toString() === product._id.toString();
+  });
+
+  // 이미 장바구니에 존재
+  if (cartProductIndex >= 0) {
+    quantity = this.cart.items[cartProductIndex].quantity + 1;
+    updateCartItem[cartProductIndex].quantity = quantity;
+  } else {
+    updateCartItem.push({
+      productId: product._id,
+      quantity,
+    });
+  }
+  const updateCart = {
+    items: updateCartItem,
+  };
+
+  this.cart = updateCart;
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updateCartItems = this.cart.items.filter((item) => {
+    console.log("item==");
+    console.log(item);
+    console.log("productId==");
+    console.log(productId);
+    return item.productId.toString() !== productId.toString();
+  });
+
+  console.log("updateCartItems===");
+  console.log(updateCartItems);
+
+  this.cart.items = updateCartItems;
+  return this.save();
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
