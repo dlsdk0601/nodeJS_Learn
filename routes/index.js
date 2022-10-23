e5c115ed71ebcb55cae3a0d6560eddd71f1476de
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-24 01:06:04 +0900

    feat: 로그인 API 라우터 등록

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import shopRoutes from "./shop/shop.js";
 import adminRoutes from "./admin/admin.js";
+import authRouter from "./auth/auth.js";
 
 const api = express.Router();
 
 api.use("/admin", adminRoutes);
 api.use(shopRoutes);
+api.use(authRouter);
 
 export default api;
