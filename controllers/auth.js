1ceae20932a4ed79a7202ddc41ed95aaa5c9db44
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-29 23:13:01 +0900

    feat: logout contorller 생성

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,29 @@
+import User from "../models/user.js";
+
 const getLogin = (req, res) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "login",
-    isAuthenticated: false,
+    isAuthenticated: req.session.isLogged,
   });
 };
 
 const postLogin = (req, res) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById("634c2663f1c2db98d820c0bb")
+    .then((user) => {
+      req.session.isLogged = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
-export default { getLogin, postLogin };
+const postLogout = (req, res) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    // 세션이 파괴됐으므로 req.session은 사용못함
+    res.redirect("/");
+  });
+};
+
+export default { getLogin, postLogin, postLogout };
