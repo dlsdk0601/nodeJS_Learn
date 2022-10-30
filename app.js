dd3b1c67311e7d7334ac377de7b002b16099d4c1
dlsdk0601 <dlsdk0601@gmail.com>
2022-10-30 23:30:41 +0900

    refactor: userId 직접적으로 넣지 말고, session.user에 따라 분기 처리하고 있으면 req.user에 대입

--- app.js
+++ app.js
@@ -57,7 +57,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("634c2663f1c2db98d820c0bb")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
