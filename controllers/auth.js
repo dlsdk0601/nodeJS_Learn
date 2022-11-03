15e3692a19571e87068e663452970aac1b22033d
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-04 00:04:41 +0900

    feat: DB에 있는 유저의 email, 비밀번호 비교해서 로그인처리

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,19 +10,33 @@ const getLogin = (req, res) => {
 };
 
 const postLogin = (req, res) => {
-  User.findById("634c2663f1c2db98d820c0bb")
+  const {
+    body: { email, password },
+  } = req;
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.isLogged = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        // save를 굳이 해주지 않아도 알아서, save 되지만
-        // 가끔 session이 DB에 저장하는 시간과 redirect 하는 도중 시간적 오차 때문에
-        // save보다 redirect가 먼저 일어나고 로그인 처리가 되지 않은 / 화면을 볼수도있음
-        // 물론 새로고침하면 다시 정상적으로 보일테지만, 한번에 처리하기 위해 save 호출하고 그안에
-        // callback 함수로 받는다.
-        console.log(err);
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((result) => {
+          if (result) {
+            req.session.isLogged = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.redirect("/login");
+        });
     })
     .catch((err) => console.log(err));
 };
