83ff3decd241332a76342550c56f4b4f7ff5baff
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-12 23:55:15 +0900

    refactor: 불필요한 isAuthenticated 삭제

--- controllers/error.js
+++ controllers/error.js
@@ -2,11 +2,10 @@ import path from "path";
 
 const __dirname = path.resolve();
 
-const get404 = (_, res) => {
+const get404 = (req, res) => {
   res.status(404).render("404", {
     pageTitle: "Page Not Found.",
     path: "/404",
-    isAuthenticated: req.isAuthenticated,
   });
 };
 
