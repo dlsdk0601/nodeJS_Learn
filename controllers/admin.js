316c3f3fe26d8bf0bd657f4cc684381e0cba6882
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-23 17:49:25 +0900

    feat: 에러에 따른 css 조건부 처리

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,6 +14,7 @@ const getAddProduct = (req, res) => {
     editing: false,
     hasError: false,
     errorMessage: null,
+    validationErros: [],
   });
 };
 
@@ -38,6 +39,7 @@ const getEditProduct = (req, res) => {
         hasError: false,
         errorMessage: null,
         product,
+        validationErrors: [],
       });
     })
     .catch((err) => console.log(err));
@@ -61,6 +63,7 @@ const postAddProduct = (req, res) => {
         price,
         description,
       },
+      validationErrors: errors.array(),
     });
   }
 
@@ -100,6 +103,26 @@ const getProducts = (req, res) => {
 
 const postEditProduct = (req, res) => {
   const { productId, title, imageUrl, price, description } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "edit-product",
+      path: "/admin/products",
+      editing: true,
+      hasError: true,
+      errorMessage: errors.array()[0].msg,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description,
+        _id: productId,
+      },
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(productId)
     .then((product) => {
