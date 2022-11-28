a4a05a2d8676a2e5d234c0465ca539ead2c96dbf
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-29 01:20:22 +0900

    fix: req 추가

--- controllers/error.js
+++ controllers/error.js
@@ -2,7 +2,7 @@ import path from "path";
 
 const __dirname = path.resolve();
 
-const get404 = (_, res) => {
+const get404 = (req, res) => {
   res.status(404).render("404", {
     pageTitle: "Page Not Found.",
     path: "/404",
